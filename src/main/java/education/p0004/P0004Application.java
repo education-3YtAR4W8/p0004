0df6a5967dfad47d41d4fa31b477ea9fbdf1d005
sasaki-jcsc <sasaki@jcsc.co.jp>
2020-08-26 15:42:10 +0900

    base

---
+++ src/main/java/education/p0004/P0004Application.java
@@ -0,0 +1,13 @@
+package education.p0004;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P0004Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P0004Application.class, args);
+	}
+
+}
---
+++ src/test/java/education/p0004/P0004ApplicationTests.java
@@ -0,0 +1,13 @@
+package education.p0004;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P0004ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
